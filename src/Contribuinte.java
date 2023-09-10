3dd3fbfb3d58c28b775ebe209b56631278e7879b
pedrolucas201 <quizueiramusj@gmail.com>
2023-09-10 14:22:51 -0300

    first class, it's a principal class, this class extends a others.

---
+++ src/Contribuinte.java
@@ -0,0 +1,30 @@
+public abstract class Contribuinte {
+
+    private  String nome;
+    private  double rendaBruta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getRendaBruta() {
+        return rendaBruta;
+    }
+
+    public void setRendaBruta(double rendaBruta) {
+        this.rendaBruta = rendaBruta;
+    }
+
+    public abstract double calcularImposto();
+
+    @Override
+    public String toString() {
+        String  s = "Nome: " + nome;
+        s += "; Renda Bruta: " + rendaBruta;
+        return  s;
+    }
+}
